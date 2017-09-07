16d016fd607b8b8e03d858775ba978b4b810696e
kl3ag2 <kl3ag2@komp03.lo1cg.org>
2017-09-07 08:43:54 +0200

    doopaaaaaaaa

--- Hello.cpp
+++ Hello.cpp
@@ -5,18 +5,45 @@
 
 using namespace std;
 
+int przyste(int n)
+{
+    int i = 0;
+    for( i=0; i<=n; i+=2)
+        cout << i << " " << endl;
+        return i / 2;
+}
+
+#define ROK_CPP 1983
+
 int main(int argc, char **argv)
 {   
     cout << "Jak Ci na imię piekny?" << endl;
     string imie;
     cin >> imie;
+    
     cout << "elo " << imie << endl;
+
     int wiek = 0;
-    cout << "Jaki jesteś stary?" << endl;
-    cin >> wiek;
-    cout << "Urodziłeś się w " << 2017-wiek << "roku. To słuszny wiek.";
+        cout << "Jaki jesteś stary?" << endl;
+        cin >> wiek;
+    
+    int rok_urodzenia = 2017-wiek;
+        cout << "Urodziłeś się w " << rok_urodzenia  << "roku. To słuszny wiek." << endl;
+    
+    if (ROK_CPP < rok_urodzenia)
+        cout << "Jesteś młodszy od cpp" << endl;
+    
+    else
+        cout << "Jesteś starszy od cpp" << endl;
+        
+    int n = 0;
+    cout << "Podaj liczbę naturalną: ";
+    cin >> n;
+    cout << "Ilość parzystych " << parzyste(n);
     
 	
-	return 0;
+	
+    
+    return 0;
 }
 
